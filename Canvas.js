1a80c1992a9613171acb1800c1c5c72bc526b4e9
Ryan Schniederjan <ryno@schnietke.com>
2023-02-08 09:21:49 -0600

    added button and tab based on Bulma CSS

---
+++ Canvas.js
@@ -0,0 +1,12 @@
+var allCode = document.getElementsByClassName("CodeSnippet");
+// const allCodeSpaces = document.getElementsByClassName("CodeWindow");
+// let counter = 0;
+// for (let theCode of allCode) {
+//   var b = theCode.innerHTML.split(">");
+//   allCodeSpaces[counter].textContent = "";
+//   for (var i = 0; i < b.length; i++) {
+//     var replaceBracket = i == b.length - 1 ? "" : ">";
+//     allCodeSpaces[counter].textContent += b[i] + replaceBracket;
+//   }
+//   counter += 1;
+// }
